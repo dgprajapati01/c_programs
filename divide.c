b36b1f893796d559aed2c154115b2d18da1968d0
dgprajapati01 <dhyey.prajapati52@gmail.com>
2026-03-03 13:37:46 +0530

    This Is My Fourth Program

---
+++ divide.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int a, b, divide;
+    printf("Enter the values :\n");
+
+    scanf("%d",&a);
+    scanf("%d",&b);
+
+    divide=a/b;
+
+    printf("The Value Of Division is :%d\n", divide);
+
+    return 0;
+}
