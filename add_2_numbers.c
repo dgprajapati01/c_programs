348b9d1c3c3df19bdeee334b6eeaab87886827c4
dgprajapati01 <dhyey.prajapati52@gmail.com>
2026-03-03 13:33:20 +0530

    This Is My Second Program

---
+++ add_2_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+
+{
+ int a, b, sum;
+ printf("Enter Values Of A and B\n");
+ scanf("%d",&a);
+ scanf("%d",&b);
+ sum = a+b;
+ printf("Entered Value Is %d\n",a);
+ printf("Entered Value is %d\n",b);
+ printf("Addition of two numbers : %d",sum);
+return 0;
+}
