ff5061caaf588ac99e99033a5c31a467a0e2fc92
dgprajapati01 <dhyey.prajapati52@gmail.com>
2026-03-06 16:22:15 +0530

    This is my Seventh Program

---
+++ area_of_square.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int side, area;
+    printf("Area of square");
+    scanf("%d",& side);
+
+    area=side*side;
+
+    print("Area of Square : %d",area);
+    return 0;
+
+}
